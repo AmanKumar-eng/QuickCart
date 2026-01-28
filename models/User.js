73392ae9f9ecaeb590857523790480e1cda2bc1c
AmanKumar-eng <kumar285aman@gmail.com>
2026-01-28 20:08:03 +0530

    Fix user duplicate creation and auth issues

--- models/User.js
+++ models/User.js
@@ -5,9 +5,9 @@ const userSchema = new mongoose.Schema({
     name: { type : String, required: true},
     email: { type : String, required: true, unique: true},
     imageUrl: { type : String, required: true},
-    cartItems: { type : Object, default: {} },
-}, { minimize: false })
+    cartItems: { type : Object, default: {} }
+}, { minimize: false , timestamps: true })
 
-const User = mongoose.models.user || mongoose.model('user',userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema)
 
-export default User
+export default User;
